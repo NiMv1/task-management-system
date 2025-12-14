e02d142d98c32d5df3e2acece0bb38cefdfe94fe
NiMv1 <bnex47@mail.ru>
2025-12-14 20:50:41 +0300

    fix: добавлен ComponentScan для common пакета (GlobalExceptionHandler)

--- auth-service/src/main/java/com/taskmanager/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/taskmanager/auth/AuthServiceApplication.java
@@ -2,11 +2,13 @@ package com.taskmanager.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Главный класс сервиса аутентификации
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.taskmanager.auth", "com.taskmanager.common"})
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
--- task-service/src/main/java/com/taskmanager/task/TaskServiceApplication.java
+++ task-service/src/main/java/com/taskmanager/task/TaskServiceApplication.java
@@ -2,11 +2,13 @@ package com.taskmanager.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Главный класс сервиса управления задачами
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.taskmanager.task", "com.taskmanager.common"})
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
